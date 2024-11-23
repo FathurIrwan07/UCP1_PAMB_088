75be906aff42e6b4facecd186afa96d3cc8f6ecf
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-11-23 15:45:27 +0700

    membuat CRUD pada table pupuk

--- app.js
+++ app.js
@@ -41,3 +41,60 @@ app.get("/bibit", (req, res) => {
     res.render("bibit", { bibit: results });
   });
 });
+
+// Tambah pupuk
+app.post("/pupuk/tambah", (req, res) => {
+  const { nama_pupuk, jenis_pupuk, harga_pupuk, stok, tanggal_masuk } =
+    req.body;
+  db.query(
+    "INSERT INTO pupuk SET ?",
+    {
+      nama_pupuk,
+      jenis_pupuk,
+      harga_pupuk,
+      stok,
+      tanggal_masuk,
+    },
+    (err) => {
+      if (err) throw err;
+      res.redirect("/pupuk");
+    }
+  );
+});
+
+// Halaman edit pupuk
+app.get("/pupuk/edit/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM pupuk WHERE id_pupuk = ?", [id], (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.render("edit_pupuk", { pupuk: results[0] }); // Kirim data pupuk ke halaman edit
+    } else {
+      res.redirect("/pupuk");
+    }
+  });
+});
+
+// Proses edit pupuk
+app.post("/pupuk/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const { nama_pupuk, jenis_pupuk, harga_pupuk, stok, tanggal_masuk } =
+    req.body;
+  db.query(
+    "UPDATE pupuk SET nama_pupuk = ?, jenis_pupuk = ?, harga_pupuk = ?, stok = ?, tanggal_masuk = ? WHERE id_pupuk = ?",
+    [nama_pupuk, jenis_pupuk, harga_pupuk, stok, tanggal_masuk, id],
+    (err) => {
+      if (err) throw err;
+      res.redirect("/pupuk");
+    }
+  );
+});
+
+// Hapus pupuk
+app.post("/pupuk/hapus/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM pupuk WHERE id_pupuk = ?", [id], (err) => {
+    if (err) throw err;
+    res.redirect("/pupuk");
+  });
+});
